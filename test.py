0b4a53231be14e38b0609b7414f3b3b664e656c9
medOblla <mohaamed.ouballa@gmail.com>
2022-07-24 02:52:21 +0100

    generating csv

--- test.py
+++ test.py
@@ -22,6 +22,7 @@ class EmailRequesAuto:
         self.email = self.config.get("login", 'email')
         self.password = self.config.get("login", "password")
         self.url = self.config.get("login", "url")
+        self.unread_msgs = []
 
     def start_driver(self):
         self.driver = webdriver.Chrome(
@@ -44,7 +45,7 @@ class EmailRequesAuto:
         loginButton.click()
         print("login")
 
-    def checkForNewEmail(self):
+    def collectUnreadUrls(self):
         self.login()
         time.sleep(8)
         table = WebDriverWait(self.driver, 3).until(
@@ -52,32 +53,47 @@ class EmailRequesAuto:
         tablebody = table.find_element(By.TAG_NAME, "tbody")
         listOFUnreadMsg = tablebody.find_elements(
             By.CSS_SELECTOR, 'tr.message')
-        a = listOFUnreadMsg[0].find_element(By.CSS_SELECTOR,
-                                            "span.subject a")
-        a.click()
-        time.sleep(7)
-        self.driver.get(a.get_attribute("href"))
-        data = []
-        x = 0
-        while x < 6:
-            email_body = self.driver.find_element(By.CLASS_NAME, "rcmBody")
-            if "Hello there!" in self.driver.find_element(By.CSS_SELECTOR, "h2.subject").text:
-                infos = email_body.find_elements(By.TAG_NAME, "p")
-                data.append({"user_id": infos[0].text.split(":")[1],
-                            "first_name": infos[1].text.split(":")[1], "last_name": infos[2].text.split(":")[1], "city": infos[3].text.split(":")[1],
-                             "postal_code": infos[4].text.split(":")[1], "country": infos[5].text.split(":")[1], "home_address": infos[6].text.split(":")[1],
-                             "email": infos[7].text.split(":")[1], "identity_type": infos[8].text.split(":")[1]})
+        for msg in listOFUnreadMsg:
+            if "Hello there!" in msg.find_element(By.CSS_SELECTOR,
+                                                  "span.subject a span").text:
+                a = msg.find_element(By.CSS_SELECTOR,
+                                     "span.subject a")
+                a.click()
+                time.sleep(3)
+                self.unread_msgs.append(a.get_attribute("href"))
 
-            self.driver.find_element(By.ID, "rcmbtn120").click()
-            x = x + 1
-        print("-----------------")
+    def checkForNewEmail(self):
+        data = []
+        self.collectUnreadUrls()
+        for msg in self.unread_msgs:
+            self.driver.get(msg)
+            email_body = self.driver.find_element(By.CLASS_NAME, "rcmBody")
+            infos = email_body.find_elements(By.TAG_NAME, "p")
+            data.append({"user_id": infos[0].text.split(":")[1],
+                         "first_name": infos[1].text.split(":")[1], "last_name": infos[2].text.split(":")[1], "city": infos[3].text.split(":")[1],
+                         "postal_code": infos[4].text.split(":")[1], "country": infos[5].text.split(":")[1], "home_address": infos[6].text.split(":")[1],
+                         "email": infos[7].text.split(":")[1], "identity_type": infos[8].text.split(":")[1]})
         df = pd.DataFrame(data)
-        df.to_csv("./jaiza_csv.csv", sep=";")
+        print("-----------------")
         print(df)
         print("-----------------")
+        df.to_csv("./jaiza_csv.csv", sep=";")
+
+    def mergeTempMails(self):
+        self.config.read('config.ini')
+        jaiza_csv_path = self.config.get("csv", 'jaiza')
+        temp_csv_path = self.config.get("csv", 'tempmail')
+        temp_csv = pd.read_csv(temp_csv_path, sep=",")
+        jaiza_csv = pd.read_csv(jaiza_csv_path, sep=";")
+
+        jaiza_csv["login_mails"] = temp_csv.mail.str.cat(temp_csv.provider)
+        jaiza_csv["phone_number"] = temp_csv["phone number"]
+        jaiza_csv["password"] = temp_csv["pswd"]
+        jaiza_csv.to_csv("./csv_final.csv", sep=";")
 
 
 if __name__ == '__main__':
     em = EmailRequesAuto()
     em.checkForNewEmail()
     em.tearDown()
+    # em.mergeTempMails()
